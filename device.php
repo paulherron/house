67d3ec0195c22228ae8744e1e4434e49e1f96bb1
Paul Herron <contact@paulherron.net>
2016-12-07 17:04:00 +0000

    Add basic output of Energenie status.

--- device.php
+++ device.php
@@ -7,9 +7,11 @@ $device = $_GET['device'];
 
 switch ($device) {
 	case 'electric-blanket':
-		$response = file_get_contents('http://rasperrypi-b.local');
+		$response = file_get_contents('http://raspberrypi-b.local');
 		break;
 	case 'attic-temperature':
 		$response = file_get_contents('http://192.168.1.111/');
 }
 
+header('Content-Type: application/json');
+echo $response;
--- js/device.js
+++ js/device.js
@@ -1,9 +1,6 @@
 $(document).ready(function() {
-	$('#front-door').html(getFrontDoorStatus);
-	$('#attic-temperature').html(getAtticTemperature);
-
+	$.getJSON('device.php?device=electric-blanket', function(data) {
+		var statusMessage = 'Turned ' + data['lastCommand'] + ' at ' + data['lastCommandTime'];
+		$('#electric-blanket').html(statusMessage);
+	});
 });
-
-var getFrontDoorStatus = function() {
-	$.json(
-}
