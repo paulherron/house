fe948ba0eb47178f65b56a9cd54de8298494c7ca
Paul Herron <contact@paulherron.net>
2016-12-08 13:31:45 +0000

    Allow toggling of device status.

--- js/device.js
+++ js/device.js
@@ -1,6 +1,32 @@
 $(document).ready(function() {
-	$.getJSON('device.php?device=electric-blanket', function(data) {
-		var statusMessage = 'Turned ' + data['lastCommand'] + ' at ' + data['lastCommandTime'];
-		$('#electric-blanket').html(statusMessage);
+	refreshStatus('electric-blanket', false);
+
+	$('.action').click(function() {
+		var url = $(this).attr('data-url');
+		var actionLink = this;
+
+		$.getJSON(url, function(data) {
+			var statusMessage = data['output'];
+			$('.secondary-status').html(statusMessage);
+
+			if (data['action'] == 'on') {
+				$(actionLink).parent().prev()
+					.addClass('positive')
+					.removeClass('negative');
+			} else {
+				$(actionLink).parent().prev()
+					.addClass('negative')
+					.removeClass('positive');
+			}
+
+			refreshStatus($(actionLink).parent().parent().attr('id'), true);
+		});
 	});
 });
+
+function refreshStatus(device, fade) {
+	$.getJSON('device.php?device=' + device, function(data) {
+		var statusMessage = 'Turned ' + data['lastCommand'] + ' at ' + data['lastCommandTime'];
+		$('#' + device + ' .status').html(statusMessage);
+	});
+}
